59b93b5ac01b79b9523a317ea9f0867c3aab1996
Leonardo Campos <leonardocampos132@gmail.com>
2022-12-21 20:47:36 -0300

    Atualizando exercicio

--- src/App.java
+++ src/App.java
@@ -11,32 +11,33 @@ public class App {
         fabri = 0.27;
         int caso;
 
-        System.out.println("Escolha o serviço que você deseja");
         System.out.println("1 - Para descobrir o custo de frabica");
         System.out.println("2 - Para descobrir o custo do carro");
+        System.out.println("3 - Para finalizar sessão");
         caso = sc.nextInt();
-        switch (caso) {
-            case 1:
-            System.out.println("Informe o valor do carro");
-            valor = sc.nextFloat();
-            
-            porc = valor * fabri;
-            System.out.println("O custo de fabrica é: R$" + porc);
-            break;
-            
-            case 2:
-            System.out.println("Informe o custo de fabrica");
+        while(caso != 3) {
+            if(caso == 1) {
+                System.out.println("Informe o valor do carro");
+                valor = sc.nextFloat();
+                
+                porc = valor * fabri;
+                System.out.println("O custo de fabrica é: R$" + porc);
+                
+            } else if (caso == 2) {
+                System.out.println("Informe o custo de fabrica");
             custoFab = sc.nextFloat();
             imposto = custoFab * 0.45;
             dist = custoFab * 0.28;
             total = custoFab + dist + imposto;
             System.out.println("O seu veiculo custará: R$" + total);
-            break;
-
-            default: 
-            System.out.println("Opção errada, tente novamente");
+            }
+            System.out.println("1 - Para descobrir o custo de frabica");
+            System.out.println("2 - Para descobrir o custo do carro");
+            System.out.println("3 - Para finalizar sessão");
+            caso = sc.nextInt();
+        }
             
-        } 
+           
 
 
     
